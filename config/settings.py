7a7b8dff4112b8ed322b2aa641caec99416fafdd
oppih-374 <letwego28@gmail.com>
2012-01-31 01:12:20 +0800

    change mysql to sqlite2 and replace qichangxing's email with mine

--- config/settings.py
+++ config/settings.py
@@ -2,15 +2,15 @@
 # coding: utf-8
 import web
 
-db = web.database(dbn='mysql', db='todo', user='root', pw='')
+db = web.database(dbn='sqlite3', db='tocome')
 
 render = web.template.render('templates/', cache=False)
 
 web.config.debug = True
 
 config = web.storage(
-    email='qichangxing@gmail.com',
-    site_name = '任务跟踪',
+    email='letwego28@gmail.com',
+    site_name = '参与人员跟踪',
     site_desc = '',
     static = '/static',
 )
