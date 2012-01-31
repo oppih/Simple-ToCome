79d492a9066fb811f58cab64d5dc404d01b30a01
oppih-374 <letwego28@gmail.com>
2012-01-31 16:18:57 +0800

    put party description information into the config

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,11 @@ config = web.storage(
     site_name = '聚餐签到表',
     site_desc = '',
     static = '/static',
+    party_time = '2012.2.1 正月初十',
+    party_place = '就在这里',
+    party_people = '象山中学2008届 高三5班',
+    party_affair = '吃、喝、聊',
+    party_notes = '请带现金过来的同学再下面选择修改，注上金额',
 )
 
 
