3d94e09ec60676d2d06384247998e9e7bc272698
oppih-374 <letwego28@gmail.com>
2012-01-31 16:24:55 +0800

    update a clear index.html

--- config/settings.py
+++ config/settings.py
@@ -18,6 +18,7 @@ config = web.storage(
     party_people = '象山中学2008届 高三5班',
     party_affair = '吃、喝、聊',
     party_notes = '请带现金过来的同学再下面选择修改，注上金额',
+    party_notes_more = '范例 ￥100 -> 选择“修改”标注金额，选择“签到”表示已到达现场',
 )
 
 
